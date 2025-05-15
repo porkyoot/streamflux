677bc06d567f6db49c21e71f4fc9f83ea974b44d
porkyoot <piggy@pig.is>
2025-05-15 06:46:29 +0200

    Adding html to public path

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
-
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Serve static files
+app.use('/public', express.static('public'));
+
+// Serve HTML pages manually
+app.get('/overlay.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'overlay.html'));
+});
+
+app.get('/dashboard.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dashboard.html'));
+});
+
+// WebSocket logic
 let overlayState = {
   weight: '???',
   fullness: 0,
@@ -20,8 +34,6 @@ io.on('connection', (socket) => {
     io.emit('update', overlayState);
   });
 });
-
-app.use(express.static('public'));
 
 server.listen(3000, () => {
   console.log('Overlay system running on http://localhost:3000');
